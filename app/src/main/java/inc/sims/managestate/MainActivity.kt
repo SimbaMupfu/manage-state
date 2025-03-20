2318a69e58448e6b37dd92a03b61d1fecba157da
SimbaMupfu <scmupfururirwa@gmail.com>
2025-03-20 10:17:39 +0200

    Create a textfield that can handle it's state.

--- app/src/main/java/inc/sims/managestate/MainActivity.kt
+++ app/src/main/java/inc/sims/managestate/MainActivity.kt
@@ -4,11 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import inc.sims.managestate.ui.theme.ManageStateTheme
@@ -19,17 +27,39 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ManageStateTheme {
-
+                Box(modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ){
+                    MyTextField()
+                }
             }
         }
     }
 }
 
+@Composable
+fun MyTextField(){
+    var textState by remember {
+        mutableStateOf("")
+    }
+
+    TextField(
+        value = textState,
+        onValueChange = {
+            textState = it
+        },
+        modifier = Modifier.fillMaxWidth()
+    )
+}
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ManageStateTheme {
-
+        Box(modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ){
+            MyTextField()
+        }
     }
 }
