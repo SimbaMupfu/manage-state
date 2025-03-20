cf5fddea5bc4ee5601b8077a2b108e33b6e1bffe
SimbaMupfu <scmupfururirwa@gmail.com>
2025-03-20 12:16:40 +0200

    Add list to display text saved by user.

--- app/src/main/java/inc/sims/managestate/MainActivity.kt
+++ app/src/main/java/inc/sims/managestate/MainActivity.kt
@@ -4,15 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -27,10 +32,29 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ManageStateTheme {
-                Box(modifier = Modifier.fillMaxSize(),
-                    contentAlignment = Alignment.Center
-                ){
-                    MyTextField()
+                Column(modifier = Modifier.fillMaxSize()){
+                    var textState by remember {
+                        mutableStateOf("")
+                    }
+                    val nameListState = remember {
+                        mutableStateListOf<String>()
+                    }
+                    LazyColumn(modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(1f)) {
+                        items(nameListState.size){
+                            Text(text = nameListState[it])
+                        }
+                    }
+                    MyTextField(
+                        textValue = textState,
+                        onValueChanged = {
+                            textState = it
+                        },
+                        onAddClick = {
+                            nameListState.add(textState)
+                            textState = ""
+                    })
                 }
             }
         }
@@ -38,28 +62,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun MyTextField(){
-    var textState by remember {
-        mutableStateOf("")
-    }
-
+fun MyTextField(textValue: String, onValueChanged: (String) -> Unit, onAddClick: () -> Unit){
     TextField(
-        value = textState,
+        value = textValue,
         onValueChange = {
-            textState = it
+            onValueChanged(it)
         },
-        modifier = Modifier.fillMaxWidth()
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    ManageStateTheme {
-        Box(modifier = Modifier.fillMaxSize(),
-            contentAlignment = Alignment.Center
-        ){
-            MyTextField()
+        modifier = Modifier.fillMaxWidth(),
+        trailingIcon = {
+            Icon(
+                imageVector = Icons.Default.Add,
+                contentDescription = null,
+                modifier = Modifier.clickable { onAddClick() }
+            )
         }
-    }
+    )
 }
